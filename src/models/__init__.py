28a53623393ff66c8f2e280e84cc08cc8c66e40e
tarekyehya <tarek_500@yahoo.com>
2024-05-11 18:59:12 +0300

    resolve git issue

---
+++ src/models/__init__.py
@@ -0,0 +1,1 @@
+from .enum.ResponseMassages import ResponseMassages
---
+++ src/models/enum/ResponseMassages.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class ResponseMassages(Enum):
+
+
+    FILE_validate_SUCCESS = "file validate success"
+    FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
+    FILE_SIZE_EXCEEDED = "file_size_exceeded"
+    FILE_UPLOD_SUCCESS = "file_upload_success"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
