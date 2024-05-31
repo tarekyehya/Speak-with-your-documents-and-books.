1e74fcaf913c96a9619a73b1b933df93bb036398
tarekyehya <tarek_500@yahoo.com>
2024-05-31 07:06:29 +0300

    mongo connection by motor

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,9 +4,13 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPEN_API_KEY: str
+    
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+    
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
 
     class Config:
         env_file = ".env"
