888d9b9fa139bd48d53c5f3a648e85b131c831e2
tarekyehya <tarek_500@yahoo.com>
2024-06-24 18:23:09 +0300

    write in db in process function

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile,status
+from fastapi import FastAPI, APIRouter, Depends, UploadFile,status, Request
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 from controllers import DataController, ProjectController,ProcessController
@@ -7,6 +7,10 @@ import aiofiles
 import os
 import logging
 from .schemes.data import ProcessRequest
+from models.ProjectModel import ProjectModel
+from models.ChunkModel import ChunkModel
+from models.db_schemes import DataChunk
+
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -72,31 +76,66 @@ async def upload_data(project_id: str, file: UploadFile,
             )    
 
 
-@data_router.post("/process/{project_id}")
-async def process_endpoint(project_id: str, process_request: ProcessRequest):
-    
+@data_router.post("/process/{project_id}") 
+async def process_endpoint(request: Request, project_id: str, process_request: ProcessRequest):
+
     file_id = process_request.file_id
     chunk_size = process_request.chunk_size
     overlap_size = process_request.overlap_size
-    
+    do_reset = process_request.do_reset
+
+    project_model = ProjectModel(
+        db_client=request.app.db_client
+    )
+
+    project = await project_model.get_project_or_create_one(
+        project_id=project_id
+    )
+
     process_controller = ProcessController(project_id=project_id)
-    
+
     file_content = process_controller.get_file_content(file_id=file_id)
-    
+
     file_chunks = process_controller.process_file_content(
         file_content=file_content,
         file_id=file_id,
         chunk_size=chunk_size,
         overlab_size=overlap_size
     )
-    
+
     if file_chunks is None or len(file_chunks) == 0:
         return JSONResponse(
-            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            status_code=status.HTTP_400_BAD_REQUEST,
             content={
-                "Massage":rs.FILE_PROCESSING_FAILED.value
+                "signal": rs.FILE_PROCESSING_FAILED.value
             }
         )
-        
-    return file_chunks 
+
+    file_chunks_records = [
+        DataChunk(
+            chunk_text=chunk.page_content,
+            chunk_metadata=chunk.metadata,
+            chunk_order=i+1,
+            chunk_project_id=project.id,
+        )
+        for i, chunk in enumerate(file_chunks)
+    ]
+
+    chunk_model = ChunkModel(
+        db_client=request.app.db_client
+    )
+
+    if do_reset == 1:
+        _ = await chunk_model.delete_chunks_by_project_id(
+            project_id=project.id
+        )
+
+    no_records = await chunk_model.insert_many_chunks(chunks=file_chunks_records)
+
+    return JSONResponse(
+        content={
+            "signal": rs.FILE_PROCESSING_SUCCESS.value,
+            "inserted_chunks": no_records
+        }
+    )
     
